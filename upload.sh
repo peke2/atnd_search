942676d0fbb8690a9cfd6ec9e32c262889651fbf
U-peke2-7\peke2 <peke2@peke2-7.(none)>
2013-03-30 15:16:57 +0900

    ファイルアップロード時のパーミッション指定を追加。

--- upload.sh
+++ upload.sh
@@ -2,4 +2,4 @@
 
 #GMO VPSへのデータコピー
 
-rsync -av --delete --exclude-from=exclude_upload.txt -e "ssh -i /home/peke2/.ssh/ssh_nopass_key" atnd_search peke2@211.125.90.40:/var/www/projects
+rsync -av --delete --chmod=Dg+rwx,Do+rx,Fa+rw,Fa-x --exclude-from=exclude_upload.txt -e "ssh -i /home/peke2/.ssh/ssh_nopass_key" atnd_search peke2@211.125.90.40:/var/www/projects
